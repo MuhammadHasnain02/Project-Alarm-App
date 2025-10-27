ce8388e5fc612e1546b1aaff9476aa048a8bc0d6
MuhammadHasnain02 <mrhasnain9009@gmail.com>
2025-10-27 13:12:07 +0500

    my-app

--- app.js
+++ app.js
@@ -21,80 +21,92 @@ setInterval(() => {
 let alarmTime = document.getElementById("alarmTime")
 let setAlarmBtn = document.getElementById("setAlarmBtn")
 let delAlarmBtn = document.getElementById("delAlarmBtn")
-
+let alarmSound = document.getElementById("alarmSound")
 let alarmList = document.getElementById("alarmList")
+
+
+
 let alarms = []
+
+// ------------ Set Alarm Button --------------
 
 setAlarmBtn.addEventListener("click" , () => {
     
     // Full time
     let timeValue = alarmTime.value
 
+    // Return empty hours & minutes
+    if (!timeValue) return alert("Please select valid time!");
+
     // Separate hours & minutes
     let [hour , minute] = timeValue.split(":")
     hour = parseInt(hour)
 
-    // Return empty hours & minutes
-    if (!timeValue) return alert("Please select valid time!");
-    
     // AM & PM handling
     let ampm = hour >= 12 ? "PM" : "AM"
     hour = hour % 12 || 12
 
     // Full time saved in alarms array
     let timeString = `${hour.toString().padStart(2, '0')}:${minute.toString().padStart(2, '0')} ${ampm}`;
-    alarms.push(timeString);
+    
+    // Duplicate alarm check
+    let alreadyExists = alarms.some(alarm => alarm.time === timeString);
+    if (alreadyExists) return alert("⏰ This alarm already exists!");
+
+    // Alarm add list (in object format)
+    alarms.push({
+        time: timeString,
+        active: true,
+        triggered: false
+    });
     console.log(alarms);
 
+    // Render alarm list
     renderAlarms()
 
-    // empty alarm Input
+    // Empty alarm Input
     alarmTime.value = ""
 
 })
 
 function renderAlarms() {
-
     alarmList.innerHTML = ""
 
-    alarms.forEach((alarm , i , iconClass) => {
+    alarms.forEach((alarm , i) => {
         
         let li = document.createElement("li")
         li.className = "flex justify-between items-center bg-white/10 border border-white/20 px-4 py-2 w-full rounded-lg";
         
         li.innerHTML = 
         `
-        <span class="text-lg font-bold text-gray-300">${alarm}</span>
+        <i class="fa-regular fa-alarm-clock text-white"></i>
+        <span class="text-lg font-bold text-gray-300">${alarm.time}</span>
         <div class="flex flex-row items-center space-x-2">
-            <button class="changeIcon text-2xl text-gray-300 hover:cursor-pointer font-bold">
-                <i class="fa-solid fa-toggle-on"></i>
+
+            <button onclick="toggleAlarm(${i})"" class="changeIcon text-[25px] text-gray-300 hover:cursor-pointer font-bold">
+                <i class="fa-solid ${alarm.active ? 'fa-toggle-on' : 'fa-toggle-off'}"></i>
             </button>
-            <button onclick="deleteAlarm(${i})" class="text-gray-300 hover:text-gray-400 hover:cursor-pointer font-bold">
+            <button onclick="deleteAlarm(${i})" class="text-[19px] text-gray-300 hover:text-gray-400 hover:cursor-pointer font-bold">
                 <i class="fa-solid fa-trash"></i>
             </button>
+
         </div>
         `
         alarmList.appendChild(li)
 
-        let changeIcon = document.querySelectorAll(".changeIcon")
-        changeIcon.forEach(btn => {
-
-            btn.addEventListener("click" , () => {
-                
-                if (btn.children[0].classList.contains("fa-toggle-on")) {
-                    btn.children[0].classList.replace("fa-toggle-on" , "fa-toggle-off")
-                }
-                else {
-                    btn.children[0].classList.replace("fa-toggle-off" , "fa-toggle-on")
-                }
-    
-            })
-
-        });
-
     });
 
 }
+
+// ------------ Toggle Alarm --------------
+
+function toggleAlarm(index) {
+    alarms[index].active = !alarms[index].active
+    alarms[index].triggered = false
+    renderAlarms();
+}
+
+// ------------ Delete Alarm --------------
 
 function deleteAlarm(index) {
     alarms.splice(index, 1);
@@ -114,22 +126,33 @@ setInterval(() => {
 
     let current = `${hrs.toString().padStart(2, '0')}:${mins.toString().padStart(2, '0')} ${ampm}`;
 
-    if (alarms.includes(current)) {
+    alarms.forEach(alarm => {
+        
+        if (alarm.active && !alarm.triggered && alarm.time === current) {
+            
+            alarm.triggered = true
 
-        alert(`⏰ Alarm for ${current} is ringing!`);
+            // --- Play alarm sound ---
+            alarmSound.currentTime = 0
+            alarmSound.play()
+    
+            // --- Alert messages ---
+            alert(`⏰ Alarm for ${current} is ringing!`);
+    
+            // --- Auto stop after 12s ---
+            setTimeout(() => {
+                
+                alarmSound.pause()
+                alarmSound.currentTime = 0
+                alarm.active = false
+                renderAlarms();
 
-        setTimeout(() => {
-            alert(`⏰ Reminder 2 for ${current}`);
-        }, 5000);
+            }, 10000);
+    
+        }
 
-        // 10 seconds baad last alert
-        setTimeout(() => {
-            alert(`⏰ Final Reminder for ${current}`);
-        }, 10000);
+    });
 
-        alarms = alarms.filter(a => a !== current);
-        renderAlarms();
 
-    }
 
 }, 1000);
